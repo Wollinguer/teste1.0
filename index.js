57a655457149182e97fdf6beea5d8d0954307fa3
Wollinguer <wollinguergabriel@gmail.com>
2026-05-26 14:29:42 -0300

    Fix fatorial function input validation and logic

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function fatorial(n) {
-  if (!Number.isinteger(n) || n<0){
-    return "Digite um número inteiro positivo";
-  }
-  let resultado = 1;
+    if (typeof n !== "number") {
+        return "Digite um número";
+    }
+    if (!Number.isInteger(n) || n < 0) {
+        return "Digite um número inteiro positivo";
+    }
 
-for (let i=1; i<= n; i++){
-  resultado *=i;
-}
-  return resultado;
-}
+    let resultado = 1;
 
+    for (let i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
 console.log(fatorial(5));
+console.log(fatorial(-2));
+console.log(fatorial(2.5));
+console.log(fatorial("abc"));
